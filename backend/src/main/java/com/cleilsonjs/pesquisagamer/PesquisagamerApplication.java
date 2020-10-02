a0ec195c2f882694797343fb65dce255ed16fae6
Cleilson José <cleilsonrock@gmail.com>
2020-10-02 11:35:09 -0300

    Project created

---
+++ backend/src/main/java/com/cleilsonjs/pesquisagamer/PesquisagamerApplication.java
@@ -0,0 +1,13 @@
+package com.cleilsonjs.pesquisagamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisagamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisagamerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cleilsonjs/pesquisagamer/PesquisagamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleilsonjs.pesquisagamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisagamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
